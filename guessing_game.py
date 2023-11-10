5d2bba8cfa41331be4591c485fc12bebc0116b83
Matthew Pantel <79867801+TypeErrorDev@users.noreply.github.com>
2023-11-09 16:24:30 -0800

    76% done?
    have the game working to a point. I need to have the median and mode printed out, then prompt the player to play again.

--- guessing_game.py
+++ guessing_game.py
@@ -5,35 +5,66 @@ Project 1 - A Number Guessing Game
 """
 
 # Import the random and statistics modules.
-# https://docs.python.org/3/library/random.html#random.randint
-# https://docs.python.org/3/library/statistics.html?highlight=statistics#module-statistics
-import statistics
+from random import randint
+from statistics import mean
+from statistics import median
+
 
 # Create the start_game function.
-
-
 def start_game():
-    print("----> Do you want to play a game? Guess the number, and you will survive the night. <----")
-    solution = []
-#   3. Continuously prompt the player for a guess.
+    # Display an intro/welcome message to the player.
+    def intro():
+        print("Do you want to play a game? Guess the number between 1-100, and you'll survive the night.")
+    intro()
+    # Store a random number as the answer/solution.
+    guesses = []
+    solution = randint(1, 100)
+    print("The solution is: {}".format(solution))
 
-#     a. If the guess is greater than the solution, display to the player "It's lower".
-#     b. If the guess is less than the solution, display to the player "It's higher".
+    # utility functions
+    def add_to_list(item):
+        guesses.append(item)
 
-#   4. Once the guess is correct, stop looping, inform the user they "Got it" and store the number of guesses it took in a list.
-#   5. Display the following data to the player
-#     a. How many attempts it took them to get the correct number in this game
-#     b. The mean of the saved attempts list
-#     c. The median of the saved attempts list
-#     d. The mode of the saved attempts list
-#   6. Prompt the player to play again
-#     a. If they decide to play again, start the game loop over.
-#     b. If they decide to quit, show them a goodbye message.
+    def list_attempts():
+        print(f"    Total Guesses: {len(guesses)}")
+
+    def list_mean():
+        guess_mean = mean(guesses)
+        print(f"    Average Guess: {guess_mean}")
+    #   3. Continuously prompt the player for a guess.
+    while True:
+        try:
+            guess = int(input("What's your guess: "))
+        except ValueError:
+            print("Sorry, please only input integers.")
+        else:
+            if guess > solution:
+                add_to_list(guess)
+                print("It's Lower")
+                continue
+            elif guess < solution:
+                add_to_list(guess)
+                print("It's Higher")
+                continue
+            elif guess > 100 or guess < 1:
+                print("Sorry, please keep the guesses between 1-100 ")
+            elif guess == solution:
+                add_to_list(guess)
+                print(
+                    "You have survived the night...This time. \nHere's some stats from the night:")
+                print(list_attempts())
+                print(list_mean())
+                break
+
+    #     c. The median of the saved attempts list
+
+    #     d. The mode of the saved attempts list
+    #   6. Prompt the player to play again
+    #     a. If they decide to play again, start the game loop over.
+    #     b. If they decide to quit, show them a goodbye message.
 
 # ( You can add more features/enhancements if you'd like to. )
 
 
 # Kick off the program by calling the start_game function.
-
-
 start_game()
