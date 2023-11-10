7f60c9346a46e967813c2eb18f3913e27a68884d
Matthew Pantel <79867801+TypeErrorDev@users.noreply.github.com>
2023-11-09 16:48:41 -0800

    updated game_stats def and marked some TODO as completed

--- guessing_game.py
+++ guessing_game.py
@@ -6,8 +6,7 @@ Project 1 - A Number Guessing Game
 
 # Import the random and statistics modules.
 from random import randint
-from statistics import mean
-from statistics import median
+import statistics
 
 
 # Create the start_game function.
@@ -21,17 +20,20 @@ def start_game():
     solution = randint(1, 100)
     print("The solution is: {}".format(solution))
 
-    # utility functions
+    # statistics function
+    def game_stats():
+        guess_mean = statistics.mean(guesses)
+        guess_median = statistics.median(guesses)
+        guess_mode = statistics.mode(guesses)
+        print(f"    Total Guesses: {len(guesses)}")
+        print(f"    Mean of your Guess: {int(guess_mean)}")
+        print(f"    Median of your Guesses: {guess_median}")
+        print(f"    Mode of your Guesses: {guess_mode}")
+
     def add_to_list(item):
         guesses.append(item)
 
-    def list_attempts():
-        print(f"    Total Guesses: {len(guesses)}")
-
-    def list_mean():
-        guess_mean = mean(guesses)
-        print(f"    Average Guess: {guess_mean}")
-    #   3. Continuously prompt the player for a guess.
+    # Continuously prompt the player for a guess.
     while True:
         try:
             guess = int(input("What's your guess: "))
@@ -41,30 +43,19 @@ def start_game():
             if guess > solution:
                 add_to_list(guess)
                 print("It's Lower")
-                continue
             elif guess < solution:
                 add_to_list(guess)
                 print("It's Higher")
-                continue
             elif guess > 100 or guess < 1:
                 print("Sorry, please keep the guesses between 1-100 ")
             elif guess == solution:
                 add_to_list(guess)
                 print(
                     "You have survived the night...This time. \nHere's some stats from the night:")
-                print(list_attempts())
-                print(list_mean())
+                game_stats()
                 break
 
-    #     c. The median of the saved attempts list
-
-    #     d. The mode of the saved attempts list
     #   6. Prompt the player to play again
     #     a. If they decide to play again, start the game loop over.
     #     b. If they decide to quit, show them a goodbye message.
-
-# ( You can add more features/enhancements if you'd like to. )
-
-
-# Kick off the program by calling the start_game function.
 start_game()
