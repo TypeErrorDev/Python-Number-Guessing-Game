344c107d10bfd8066972a99bb6d193b0364c4891
typeerrordev <matthew.pantel86@gmail.com>
2023-11-22 10:29:23 -0700

    High score, game stats, new game prompt
    High score is working. Need to get it to carry over to new game
    game stats are working as well, but need to carry over
    and the new game prompt - Need the new input line created and to create a new randint variable so that a new number is chosen

--- guessing_game.py
+++ guessing_game.py
@@ -15,12 +15,12 @@ def start_game():
     # Display an intro/welcome message to the player.
 
     def intro():
-        print("Do you want to play a game? Guess the number between 1-100, and you'll survive the night.")
+        print("Lets play a game. If you guess my number that is between 1-100, you'll win the game.\nGuess wrong, and....yup you guessed it...you'll lose the game.")
     intro()
 
     guesses = []
     # TODO Need to display a high score, or a "No High Score"
-    tries = []
+    high_score = []
     solution = randint(1, 100)
     print(f"The solution is: {solution}")
 
@@ -30,6 +30,8 @@ def start_game():
         guess_mean = mean(guesses)
         guess_median = median(guesses)
         guess_mode = mode(guesses)
+        high_score.append(len(guesses))
+        print(f"    Here's your high score: {min(high_score)}")
         print(f"    Total Guesses: {len(guesses)}")
         print(f"    Mean of your Guess: {int(guess_mean)}")
         print(f"    Median of your Guesses: {int(guess_median)}")
@@ -39,29 +41,48 @@ def start_game():
         guesses.append(item)
         guesses.sort()
 
+
+    def play_again():
+        while True:
+            play_again = input("Let's play again? Y/N: ")
+            if play_again.lower() == "y":
+                print(f"Here is your current High Score: {min(high_score)}\nLet's see if you can beat it!")
+                break
+            elif play_again.lower() == "n":
+                print("OK. Bye!")
+                return
+            else:
+                print("Please enter 'Y' or 'N'.")  
+                 
+
     # Continuously prompt the player for a guess.
     while True:
         try:
-            guess = int(input("What's your guess: "))
+            guess = int(input("So, what's your guess: "))
         except ValueError:
             print("Sorry, please only input integers.")
         else:
             if guess > 100 or guess < 1:
                 print("Sorry, please keep the guesses between 1-100 ")
+                continue
             elif guess > solution:
                 add_to_list(guess)
                 print("It's Lower")
+                continue
             elif guess < solution:
                 add_to_list(guess)
                 print("It's Higher")
+                continue
             elif guess == solution:
                 add_to_list(guess)
                 print(
                     "You have survived the night...This time. \nHere's some stats from the night:")
                 game_stats()
-                print(tries)
+                play_again()
                 break
-        # play_again = input("Would you like to play another game? (Yes/No): ")
+                
+
+
 
     # TODO Need to complete the below
     #   6. Prompt the player to play again
